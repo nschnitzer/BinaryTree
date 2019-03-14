4d491ffa10b5c088ce935e38a588897bccde71c2
Nathan Schnitzer <120NSchnitzer@frhsd.com>
2019-03-14 14:06:13 -0400

    Still issue with insert; finished a bunch of stuff

--- src/Tree/BinaryTree.java
+++ src/Tree/BinaryTree.java
@@ -7,7 +7,10 @@
 
 
 package Tree;
-import java.lang.Thread;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import Queue.Queue;
 
 public class BinaryTree<T>
@@ -111,7 +114,7 @@ public class BinaryTree<T>
 			lowestIndex = currentLoc.getLeft();
 			return;
 		}
-		currentLoc = lowestIndex.getParent();
+		
 		if (currentLoc.hasLeft() && currentLoc.hasRight())
 		{
 			if (currentLoc.equals(currentLoc.getParent().getRight()))
@@ -126,11 +129,30 @@ public class BinaryTree<T>
 			lowestIndex = currentLoc.getLeft();
 			return;
 		}
+		
 		currentLoc.setRight(new TreeNode<T>(n, null, null, currentLoc));
 		lowestIndex = currentLoc.getRight();
 		
 	}
 	
+	public void insert2(T t)
+	{
+		if (root == null)
+		{
+			root = new TreeNode<T>(t, null, null);
+			lowestIndex = root;
+			determineHeight();
+			return;
+		}
+		
+		
+		TreeNode<T> index = root;
+		
+		if (index.hasLeft() && !index.hasRight())
+		{
+			
+		}
+	}
 	
 	//Gets parent of left most element
 	public TreeNode<T> getDeepParent(TreeNode<T> index)
@@ -147,60 +169,34 @@ public class BinaryTree<T>
 	{
 		TreeNode<T> node = root;
 		Queue<T> que = new Queue<T>();
-		que.push(node.getData());		
-		if (!(node.hasChildren()))
+		que.push(node.getData());
+		if (node.hasLeft())
 		{
-			que.printQueue();
-			return;
-		}
-		if (node.hasRight() == false)
-		{		
 			BreadthFirstSearch(node.getLeft(), que);
 		}
-		
-		BreadthFirstSearch(node.getLeft(), que);
-		BreadthFirstSearch(node.getRight(), que);
+		if (node.hasRight())
+		{
+			BreadthFirstSearch(node.getRight(), que);
+		}
 		
 		System.out.println("OUT:");
 		que.printQueue();
-		
-		System.out.println();
-		System.out.println();
-		System.out.println(Math.ceil((Math.log10(que.getLength())/Math.log10(2))));
-		for (int i = 0; i <(int) Math.ceil((Math.log10(que.getLength())/Math.log10(2))); i++)
-		{
-			//System.out.println(Math.pow(2, i));
-			for (int k = 0; k < (int) Math.pow(2, i); k++)
-			{
-				if (que.peek() == null)
-				{
-					System.out.print("--" + "\t");
-					continue;
-				}
-				System.out.print(que.pop() + "\t");
-			}
-			System.out.println();
-		}
 			
 	}
 	
 	private void BreadthFirstSearch(TreeNode<T> node, Queue<T> q)
 	{
 		q.push(node.getData());
-		if (!(node.hasChildren()))
-		{
-			return;
-		}
-		
-		if (node.hasRight() == false)
+		if (node.hasLeft())
 		{
 			BreadthFirstSearch(node.getLeft(), q);
-			return;
 		}
-		BreadthFirstSearch(node.getLeft(), q);
-		BreadthFirstSearch(node.getRight(), q);
-		return;
+		if (node.hasRight())
+		{
+			BreadthFirstSearch(node.getRight(), q);
+		}
 		
+		return;
 		
 	}
 	
@@ -230,5 +226,162 @@ public class BinaryTree<T>
 		getGivenLength(node.getLeft(), l+1, target, q);
 		getGivenLength(node.getRight(), l+1, target, q);
 	}
+	
+	public void printPreOrder()
+	{
+		TreeNode<T> node = root;
+		Queue<T> que = new Queue<T>();
+		
+		que.push(node.getData());
+		
+		if (node.hasLeft())
+		{
+			printPreOrder(node.getLeft(), que);
+		}
+		
+		if (node.hasRight())
+		{
+			printPreOrder(node.getRight(), que);
+		}
+		
+		que.printQueue();
+		
+	}
+	
+	private void printPreOrder(TreeNode<T> node, Queue<T> que)
+	{
+		que.push(node.getData());
+		if (node.hasLeft())
+		{
+			printPreOrder(node.getLeft(), que);
+		}
+		if (node.hasRight())
+		{
+			printPreOrder(node.getRight(), que);
+		}
+		return;
+	}
+	
+	public void printPostOrder()
+	{
+		TreeNode<T> node = root;
+		Queue<T> que = new Queue<T>();
+		
+		if (node.hasLeft())
+		{
+			printPostOrder(node.getLeft(), que);
+		}
+		
+		if (node.hasRight())
+		{
+			printPostOrder(node.getRight(), que);
+		}
+		
+		que.push(node.getData());
+		
+		que.printQueue();
+	}
+	
+	private void printPostOrder(TreeNode<T> node, Queue<T> que)
+	{
+		if (node.hasLeft())
+		{
+			printPostOrder(node.getLeft(), que);
+		}
+		
+		if (node.hasRight())
+		{
+			printPostOrder(node.getRight(), que);
+		}
+		
+		que.push(node.getData());
+	}
+
+}
+
+class BTreePrinter {
+
+    public static <T extends Comparable<?>> void printNode(TreeNode<T> root) {
+        int maxLevel = BTreePrinter.maxLevel(root);
+
+        printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+    }
+
+    private static <T extends Comparable<?>> void printNodeInternal(List<TreeNode<T>> nodes, int level, int maxLevel) {
+        if (nodes.isEmpty() || BTreePrinter.isAllElementsNull(nodes))
+            return;
+
+        int floor = maxLevel - level;
+        int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+        int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+        int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+        BTreePrinter.printWhitespaces(firstSpaces);
+
+        List<TreeNode<T>> newNodes = new ArrayList<TreeNode<T>>();
+        for (TreeNode<T> node : nodes) {
+            if (node != null) {
+                System.out.print(node.data);
+                newNodes.add(node.left);
+                newNodes.add(node.right);
+            } else {
+                newNodes.add(null);
+                newNodes.add(null);
+                System.out.print(" ");
+            }
+
+            BTreePrinter.printWhitespaces(betweenSpaces);
+        }
+        System.out.println("");
+
+        for (int i = 1; i <= endgeLines; i++) {
+            for (int j = 0; j < nodes.size(); j++) {
+                BTreePrinter.printWhitespaces(firstSpaces - i);
+                if (nodes.get(j) == null) {
+                    BTreePrinter.printWhitespaces(endgeLines + endgeLines + i + 1);
+                    continue;
+                }
+
+                if (nodes.get(j).left != null)
+                    System.out.print("/");
+                else
+                    BTreePrinter.printWhitespaces(1);
+
+                BTreePrinter.printWhitespaces(i + i - 1);
+
+                if (nodes.get(j).right != null)
+                    System.out.print("\\");
+                else
+                    BTreePrinter.printWhitespaces(1);
+
+                BTreePrinter.printWhitespaces(endgeLines + endgeLines - i);
+            }
+
+            System.out.println("");
+        }
+
+        printNodeInternal(newNodes, level + 1, maxLevel);
+    }
+
+    private static void printWhitespaces(int count) {
+        for (int i = 0; i < count; i++)
+            System.out.print(" ");
+    }
+
+    private static <T extends Comparable<?>> int maxLevel(TreeNode<T> node) {
+        if (node == null)
+            return 0;
+
+        return Math.max(BTreePrinter.maxLevel(node.left), BTreePrinter.maxLevel(node.right)) + 1;
+    }
+
+    private static <T> boolean isAllElementsNull(List<T> list) {
+        for (Object object : list) {
+            if (object != null)
+                return false;
+        }
+
+        return true;
+    }
 
 }
--- src/Tree/TreeDriver.java
+++ src/Tree/TreeDriver.java
@@ -4,16 +4,37 @@ public class TreeDriver {
 
 	public static void main(String[] args) 
 	{
-		BinaryTree<String> bt = new BinaryTree<String>();
-		bt.insert("Hello");
-		bt.insert("Hi There");
-		bt.insert("Whats up");
-		bt.insert("Hi");
-		bt.insert("Hola");
+		BinaryTree<Integer> bt = new BinaryTree<Integer>();
+		bt.insert(1);
+		bt.insert(2);
+		bt.insert(3);
+		bt.insert(4);
+		bt.insert(5);
 		System.out.println(bt.getHeight());
 		bt.BreadthFirstSearch();
+		//bt.getGivenLength(2);
 		System.out.println();
+		bt.printPreOrder();
+		System.out.println();
+		BTreePrinter.printNode(bt.root);
+		System.out.println();
+		bt.printPostOrder();
 	//	bt.getGivenLength(2);
+		
+		
+		/*
+		 * WORKS:
+		 * 
+		 * BreadthFirstSearch
+		 * getGivenLength
+		 * height
+		 * PreOrder
+		 * 
+		 * TODO:
+		 * InOrder
+		 * 
+		 * ISSUE WITH INSERT - Attempting Redesign
+		 */
 
 	}
 
